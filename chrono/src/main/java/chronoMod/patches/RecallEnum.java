d2a031b7f1dc8846afc31e33fe582e9113342a41
Bin Xuan Kong <binxuankong@gmail.com>
2021-10-06 18:26:37 +0800

    Add beta card images

--- chrono/src/main/java/chronoMod/patches/RecallEnum.java
+++ chrono/src/main/java/chronoMod/patches/RecallEnum.java
@@ -1,9 +1,15 @@
 package chronoMod.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
 public class AbstractPowerEnum {
     @SpireEnum
     public static AbstractPower.PowerType RECALL;
+}
+
+public class RecallPowerEnum {
+    @SpireEnum
+    public static DamageInfo.DamageType RECALL;
 }
